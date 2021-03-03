e2db1cb300ae2112d3e479d7ac3cb23377d7b1b6
CaptainOfPhB <captaininphw@gmail.com>
2021-03-03 16:31:14 +0800

    stash

---
+++ keyboard.js
@@ -0,0 +1,34 @@
+window.keyboard_data = [
+  {
+    "q": "qq.com",
+    "w": "weibo.com",
+    "e": "ele.me",
+    "r": "renren.com",
+    "t": "twitter.com",
+    "y": "youtube.com",
+    "u": "ubuntu.com",
+    "i": "iciba.com",
+    "o": "office.com",
+    "p": "processon.com"
+  },
+  {
+    "a": "alipay.com",
+    "s": "shadowsocks.org",
+    "d": "deepin.org",
+    "f": "facebook.com",
+    "g": "github.com",
+    "h": "hackerrank.com",
+    "j": "jirengu.com",
+    "k": "kugou.com",
+    "l": "leetcode.com"
+  },
+  {
+    "z": "zybuluo.com",
+    "x": "xunlei.com",
+    "c": "ctrip.com",
+    "v": "vip.com",
+    "b": "baidu.com",
+    "n": "nodejs.org",
+    "m": "material-ui.com"
+  }
+];
